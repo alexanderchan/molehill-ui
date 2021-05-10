cf73a390174fbae539de5a00c1e2a42a1bf64646
Wes Cole <wesley.cole87@gmail.com>
2021-05-10 14:47:40 -0400

    Update css types

--- packages/babel-plugin/types/css-prop.d.ts
+++ packages/babel-plugin/types/css-prop.d.ts
@@ -1,7 +1,18 @@
 import {} from 'react'
+import * as CSS from 'csstype'
+
+declare namespace molehill {
+  export interface CSSAttribute extends CSS.Properties {
+    [key: string]: CSSAttribute | string | number | undefined
+  }
+}
+
+export default molehill
+export as namespace molehill
 
 declare module 'react' {
   interface Attributes {
-    css?: any
+    css?: molehill.CSSAttribute
+    style?: molehill.CSSAttribute
   }
 }
